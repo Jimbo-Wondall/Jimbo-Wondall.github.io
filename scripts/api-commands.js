b27a9f9444fbcf8aa9115707e24bf95754b9db8e
Jimbo-Wondall <109582514+Jimbo-Wondall@users.noreply.github.com>
2023-03-31 00:20:45 +1000

    Updated and refactored query building

--- scripts/api-commands.js
+++ scripts/api-commands.js
@@ -1,65 +1,75 @@
-//const baseURL = 'https://weatherdatawebapi.azurewebsites.net/api';
-const baseURL = 'https://localhost:7112/api';
-var savedUsers = new Array();
+const baseURL = 'https://weatherdatawebapi.azurewebsites.net/api';
+//const baseURL = 'https://localhost:7112/api';
+
+// TODO need to set combo box content programatically
+var savedUsers = JSON.parse(window.sessionStorage.getItem("users"));
+
+function handleForm(formEl) {
+	let values = {};
+	const inputs = Combine(formEl.getElementsByTagName('input'), formEl.getElementsByTagName('select'));
+	for (let i = 0; i < inputs.length; i++) {
+		let key = inputs[i].dataset.valueType;
+		let value = inputs[i].value
+		values[key] = value;
+	}
+	console.log(values);
+
+	switch(formEl.dataset.page) {
+		case 'GetSensor':
+			
+		  break;
+		case 'CreateUser': 
+			let user = {
+				username: values.username,
+				email: values.email,
+				role: values.role
+			}
+			let endpoint = '/User/CreateUser';
+			if (values.role == "Guest") endpoint = '/User/CreateGuest';
+			let response = sendQuery('POST', endpoint, values.apiKey, null, user);
+			console.log(response);
+		break;
+		default: break;
+	  }
+}
+
+function Combine(collection1, collection2){
+	var ary=[];
+	for (var i = 0; i < collection1.length; i++){
+		ary.push(collection1[i]);
+	}
+	for (var i = 0; i < collection2.length; i++){
+		ary.push(collection2[i]);
+	}
+	return ary;
+}
+
+async function sendQuery(methodInput, endpoint, authorKey, queryParams, payloadObj) {
+	let data = new FormData();
+	let params = "";
+	if(queryParams) params = '/' + new URLSearchParams(queryParams).toString();
+	if(payloadObj) data.append("json", JSON.stringify(payloadObj));
+	let myResponse = await fetch(
+		baseURL + endpoint + params, 
+		{ 
+			method: methodInput, 
+			headers: {'Authorization' : authorKey},
+			body: JSON.stringify(data)
+		}
+	);
+	console.log(myResponse);
+	return myResponse.then(r => r.json());
+}
 
 function AddUser(user) {
 	savedUsers.push(user);
 	SaveUsersToStorage();
 }
 
-function LoadUsersFromStorage() {
-	savedUsers = JSON.parse(window.sessionStorage.getItem("users"));
-	
-}
 function SaveUsersToStorage() {
 	window.sessionStorage.setItem("users", JSON.stringify(savedUsers));
 }
 
-async function GetAll(apiKey){
-	apiKey = '0c7a80a6-c90f-4757-8e3d-4e81d8a0e8a6';
-	let myResponse = JSON.parse(await fetch(baseURL + '/SensorData', { method: 'GET', headers: {'Authorization': apiKey }}).then(r => r.json()));
-	console.log(myResponse);
-}
-
-async function GetUserInfo(apiKey, requestedKey) {
-	// for "logging in" as a user, query api to see if user exists
-	// should not need apikey
-	let myResponse = JSON.parse(await fetch(baseURL + '/Users', { method: 'GET', headers: {'Authorization': apiKey, 'Requested GUID': requestedKey }}).then(r => r.json()));
-	console.log(myResponse);
-	// if user exists store info returned in session storage
-	//var responseHeaders = myResponse.headers
-	//var user = {
-	//	username: responseHeaders.Username,
-	//	guid: responseHeaders.ApiKey
-	//};
-	//AddUser(user);
-	
-}
-
-async function CreateNewUser() {
-	var apiKey = '518997c0-88d7-4e8b-8cd8-9df7f0efa8dd';
-	var userInfo = {
-		username: "test account",
-		email: "example@email.com",
-		role: "Guest",
-		lastLogin: "2023-03-10T13:29:52.883Z",
-		apiKey: "0c7a80a6-c90f-4757-8e3d-4e81d8a0e8a6"
-	};
-	let user = JSON.stringify(userInfo);
-	let myResponse = await fetch(baseURL + '/Users/CreateUser', { 
-		method: 'POST', 
-		headers: 
-		{
-			'Authorization': apiKey 
-		}, 
-		body: user
-	}).then(r => r.json());
-	console.log(myResponse);
-	// if response was successfull, store user info and returned guid in session storage
-	//var responseHeaders = myResponse.headers
-	//var user = {
-	//	username: responseHeaders.Username,
-	//	guid: responseHeaders.ApiKey
-	//};
-	//AddUser(user);
+function LoadUsersFromStorage() {
+	savedUsers = JSON.parse(window.sessionStorage.getItem("users"));
 }
