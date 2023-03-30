fedd76f8f674646c7cc0473f82bf098c148ea131
Jimbo-Wondall <109582514+Jimbo-Wondall@users.noreply.github.com>
2023-03-31 01:22:22 +1000

    Fixed query builder issues

--- scripts/api-commands.js
+++ scripts/api-commands.js
@@ -20,12 +20,12 @@ function handleForm(formEl) {
 		  break;
 		case 'CreateUser': 
 			let user = {
-				username: values.username,
-				email: values.email,
-				role: values.role
+				"username": values.Username,
+				"email": values.Email,
+				"role": values.Role
 			}
 			let endpoint = '/User/CreateUser';
-			if (values.role == "Guest") endpoint = '/User/CreateGuest';
+			if (values.Role == 'Guest') endpoint = '/User/CreateGuest';
 			let response = sendQuery('POST', endpoint, values.apiKey, null, user);
 			console.log(response);
 		break;
@@ -45,20 +45,53 @@ function Combine(collection1, collection2){
 }
 
 async function sendQuery(methodInput, endpoint, authorKey, queryParams, payloadObj) {
-	let data = new FormData();
 	let params = "";
 	if(queryParams) params = '/' + new URLSearchParams(queryParams).toString();
-	if(payloadObj) data.append("json", JSON.stringify(payloadObj));
-	let myResponse = await fetch(
+
+	console.log(methodInput.toString());
+	console.log(authorKey.toString());
+	console.log(baseURL + endpoint + params);
+
+	return await fetch(
 		baseURL + endpoint + params, 
 		{ 
-			method: methodInput, 
-			headers: {'Authorization' : authorKey},
-			body: JSON.stringify(data)
+			method: methodInput.toString(), 
+			headers: {
+				'Authorization' : authorKey.toString(),
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(payloadObj)
 		}
 	);
-	console.log(myResponse);
-	return myResponse.then(r => r.json());
+	//.then((r) => r.json()).then((d) => console.log(d));
+	//return myResponse;
+}
+
+async function exampleQuery() {
+	return await fetch(
+		"https://localhost:7112/api/SensorData", 
+		{
+		method: 'POST', 
+		headers: {
+			'Authorization': '518997c0-88d7-4e8b-8cd8-9df7f0efa8dd', 
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			"deviceName": "string",
+			"precipitation": 0,
+			"time": "2023-03-23T01:52:09.648Z",
+			"latitude": 2.70507,
+			"longitude": 90.7559,
+			"temperature": 20,
+			"atmosphericPressure": 120.05,
+			"maxWindSpeed": 5,
+			"solarRadiation": 590,
+			"vaporPressure": 1.8,
+			"humidity": 65,
+			"windDirection": 154.47 
+		})
+	})
+	//.then((r) => r.json()).then((d) => console.log(d));
 }
 
 function AddUser(user) {
